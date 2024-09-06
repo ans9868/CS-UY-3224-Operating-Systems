71e9623c73f7196631b423145865d44a40739871
Lubuntu VM <ans9868@nyu.edu>
2024-09-06 00:11:03 -0400

    finished assignment1

---
+++ homework/assignment1/lab1_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+    int randomNum = rand() % 200;
+    printf("Hello world! This is CS2334, Fall 2024! \nAdel Sahuc\n%d\n", randomNum);
+    return 0;
+	
+}
+
